06ac6aebc93eafa0afcae78def02730a1a0bb038
Krisadaporn Petchdeee <krisadaporn@Krisadaporns-MacBook-Air.local>
2025-09-30 21:31:05 +0700

    update font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -525,7 +525,7 @@ export default function WeddingPage() {
       {showRsvpForm && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 animate-fade-in">
           <div className="bg-white p-8 rounded-lg max-w-md w-full mx-4 transform animate-scale-in">
-            <h3 className="text-2xl  text-center mb-6">{copy[lang].rsvpForm.heading}</h3>
+            <h3 className={`text-2xl  text-center mb-6 ${lang === "en" ? "font-en" : "font-th"}`}>{copy[lang].rsvpForm.heading}</h3>
             <form onSubmit={handleSubmit} className="space-y-4">
               <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
                 
